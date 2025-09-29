cfba38194e9f1ad854546bacd9b4689fb1438482
vengadeshraj <vengadeshraj405@gmail.com>
2025-09-29 22:43:30 +0530

    fetch books service added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,35 @@
-
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import {typeDefBooks} from './schema.js'
+import { typeDefBooks } from "./schema.js";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
-async function startApolloServer(){
-    const server = new ApolloServer({typeDefs:typeDefBooks});
-    const {url} = await startStandaloneServer(server);
-    console.log(`success ${url}`)
+const mocks = {
+  Query: () => ({
+    bookStore: () => [...new Array(6)],
+  }),
+  Book: () => ({
+    id: () => "book_01",
+    name: () => "Race of my life",
+    author: () => {
+      return {
+        id: "author_01",
+        name: "milkha singh",
+      };
+    },
+    description: () => "Milkha singh's life story",
+  }),
 };
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs: typeDefBooks }),
+      mocks,
+    }),
+  });
+  const { url } = await startStandaloneServer(server);
+  console.log(`success ${url}`);
+}
 
 startApolloServer();
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,19 +1,19 @@
-import gql from "graphql-tag";  
- 
+import gql from "graphql-tag";
+
 export const typeDefBooks = gql`
   type Query {
     bookStore: [Book!]!
   }
-   
+
   type Book {
     id: ID!
     name: String!
     author: Author!
     description: String
   }
-   
+
   type Author {
     id: ID!
     name: String!
   }
-`;  
+`;
