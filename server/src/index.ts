2edf1592247b85d25c614db78745e314052f3427
vengadeshraj <vengadeshraj405@gmail.com>
2025-09-29 22:31:26 +0530

    create apollo server added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
 
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import {typeDefBooks} from './schema.js'
+
+async function startApolloServer(){
+    const server = new ApolloServer({typeDefs:typeDefBooks});
+    const {url} = await startStandaloneServer(server);
+    console.log(`success ${url}`)
+};
+
+startApolloServer();
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,6 @@
-import gql from "graphql-tag";
+import gql from "graphql-tag";  
  
-export const typeDefs = gql`
+export const typeDefBooks = gql`
   type Query {
     bookStore: [Book!]!
   }
